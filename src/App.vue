e8cb59b15b422c236ec9b2ec852c22c1e81c95f1
lordevito <lollo.devito@hotmail.com>
2024-02-16 00:58:12 +0100

    feat: chiamata API axios, appmain e project card

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,47 @@
 <script>
+//importo AppHeader
 import AppHeader from "./components/AppHeader.vue";
+//importo AppMain
+import AppMain from "./components/AppMain.vue";
+//importo axios
+import axios from "axios";
 export default {
   name: "Boolfolio",
+  data() {
+    return {
+      baseUrl: "http://127.0.0.1:8000",
+      apiUrls: {
+        projects: "/api/projects",
+      },
+      projects: [],
+    };
+  },
   components: {
     AppHeader,
+    AppMain,
+  },
+  methods: {
+    getProjects() {
+      axios
+        .get(this.baseUrl + this.apiUrls.projects)
+        .then((response) => {
+          console.log(response.data.data);
+          this.projects = response.data.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.getProjects();
   },
 };
 </script>
 
 <template>
-  <div>prova</div>
   <AppHeader />
+  <AppMain :projects="projects" />
 </template>
 
 <style></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+<template>
+  <header>
+    <div class="container d-flex justify-content-between bg-secondary text-white p-3">
+      <div>Immagine</div>
+      <nav>
+        <ul class="list-unstyled p-0 m-0 d-flex gap-4">
+          
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+<style></style>
